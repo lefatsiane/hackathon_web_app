705401e5c734b303db354de343c47e7489117ecb
lefatsiane <154235500+lefatsiane@users.noreply.github.com>
2026-09-15 11:18:43 +0200

    eslint configuration

--- gradurat/eslint.config.js
+++ gradurat/eslint.config.js
@@ -1,5 +1,5 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
+const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
@@ -7,8 +7,8 @@ module.exports = defineConfig([
   {
     ignores: ["dist/*"],
     rules: {
-      'react-hooks/set-state-in-effect': 'off',
-      'react/no-unescaped-entities': 'off',
+      "react-hooks/set-state-in-effect": "off",
+      "react/no-unescaped-entities": "off",
     },
-  }
+  },
 ]);
